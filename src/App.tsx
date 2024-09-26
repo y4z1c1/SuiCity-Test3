f11ccb14c025c92e40fbc818d45a0b9322618037
nft entreprenaur <56488393+yay61@users.noreply.github.com>
2024-09-26 20:58:39 +0300

    maint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Game from "./components/Game";
+// import Game from "./components/Game";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SuccessPage from "./components/SuccessPage";
 
